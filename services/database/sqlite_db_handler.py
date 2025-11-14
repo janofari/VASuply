64affe826f9b3da5eb8ec34249d04356fa724439
Ricardo <ricardo.macias.leiva@gmail.com>
2025-11-15 00:37:20 +0100

    corrected db path for raspberry

--- services/database/sqlite_db_handler.py
+++ services/database/sqlite_db_handler.py
@@ -4,7 +4,7 @@ import sqlite3
 from flask import session
 from werkzeug.security import check_password_hash, generate_password_hash
 
-USER_DB_PATH = r"C:\Users\ricar\OneDrive\Documentos\GitHub\VASuply\services\database\users_sqlite.db"
+USER_DB_PATH = r"/home/ricardoml95/Documents/GitProjects/VASuply/services/database/users_sqlite.db"
 
 
 def connect_db():
